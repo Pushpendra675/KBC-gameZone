cf3ddf4482872f2261bc5dda53e4fd32da6de7b8
pushpendra675 <pushpendrapatil23@gmail.com>
2026-06-17 10:20:58 +0530

    My KBC Project

---
+++ oop.py
@@ -0,0 +1,14 @@
+# from abc import ABC, abstractmethod
+
+# class Vehicle(ABC):
+#     @abstractmethod   #annotation
+#     def start(self):
+#         pass
+# class car(Vehicle):
+#     def start(self):
+#         print("Car Started")
+
+
+# c = car()
+# c.start()
+
